e9dd630ac5520e9310518bd5569e803306369900
Danila Egorov <c4nsero@mail.ru>
2022-05-17 17:01:46 +0300

    add 0 version
    Набросал для начала пару штрихов

---
+++ main.go
@@ -0,0 +1,113 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"io/ioutil"
+	"log"
+	"net/http"
+	"strings"
+	"time"
+
+	"github.com/gorilla/mux"
+)
+
+// Структура для хранения полной информации о пользователе
+type UserInfo struct {
+	UserID   string `json:"id"`
+	Username string `json:"username"`
+}
+
+// Функция поиска. Возвращает искомое значение и индекс
+func find(str string, subStr string, char byte) (string, int) {
+
+	// Поиск индекса начала нужной строки
+	left := strings.Index(str, subStr) + len(subStr)
+
+	// Проверка на существование нужной строки
+	if left > len(subStr)-1 {
+
+		// Крайняя часть искомой строки
+		right := left
+
+		for ; str[right] != char; right++ {
+			// Доводит str[right] до символа char
+		}
+		return str[left:right], right
+	}
+
+	return "", 0
+}
+
+// Функция получения информации о пользователе
+func getUserInfo(id string) UserInfo {
+
+	// Формирование и исполнение запроса
+	resp, err := http.Get("https://osu.ppy.sh/users/" + id)
+	if err != nil {
+		return UserInfo{}
+	}
+
+	// Запись респонса
+	defer resp.Body.Close()
+	body, _ := ioutil.ReadAll(resp.Body)
+
+	// HTML полученной страницы в формате string
+	pageStr := string(body)
+
+	// Сохранение html'ки в файл sample.html
+	/*if err := os.WriteFile("sample.html", body, 0666); err != nil {
+		log.Fatal(err)
+	}*/
+
+	// Структура, которую будет возвращать функция
+	result := UserInfo{
+		UserID: id,
+	}
+
+	// Индекс конца последней найденной строки
+	i := 0
+
+	/* -----------------------------------------------------------
+	# Далее происходит заполнение полей функцией find			 #
+	# после каждого поиска тело сайта обрезается для оптимизации #
+	------------------------------------------------------------ */
+
+	// Репозитории
+	result.Username, i = find(pageStr, "<title>", ' ')
+	pageStr = pageStr[i:]
+
+	return result
+}
+
+// Функция отправки информации о пользователе
+func sendUserInfo(writer http.ResponseWriter, request *http.Request) {
+
+	// Заголовок, определяющий тип данных респонса
+	writer.Header().Set("Content-Type", "application/json")
+
+	// Обработка данных и вывод результата
+	json.NewEncoder(writer).Encode(getUserInfo(mux.Vars(request)["id"]))
+}
+
+func main() {
+
+	// Вывод времени начала работы
+	fmt.Println("API Start: " + string(time.Now().Format("2006-01-02 15:04:05")))
+
+	// Роутер
+	router := mux.NewRouter()
+
+	// Маршруты
+
+	router.HandleFunc("/user/{id}", sendUserInfo).Methods("GET")
+	router.HandleFunc("/user/{id}/", sendUserInfo).Methods("GET")
+
+	// Запуск API
+
+	// Для Heroku
+	//log.Fatal(http.ListenAndServe(":"+os.Getenv("PORT"), router))
+
+	// Для локалхоста (127.0.0.1:8080/)
+	log.Fatal(http.ListenAndServe(":8080", router))
+}
