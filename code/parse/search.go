2d2ea8af27d922d5f35ac477de169ae30da547c6
hud0shnik <c4nsero@mail.ru>
2022-10-01 15:43:18 +0300

    update search.go
    Файл с функциями поиска

---
+++ code/parse/search.go
@@ -0,0 +1,52 @@
+package parse
+
+import (
+	"strings"
+)
+
+// Функция поиска. Возвращает искомое значение и индекс последнего символа
+func findWithIndex(str, subStr, stopChar string, start int) (string, int) {
+
+	// Обрезка левой границы поиска
+	str = str[start:]
+
+	// Проверка на существование нужной строки
+	if strings.Contains(str, subStr) {
+
+		// Поиск индекса начала нужной строки
+		left := strings.Index(str, subStr) + len(subStr)
+
+		// Поиск правой границы
+		right := left + strings.Index(str[left:], stopChar)
+
+		// Обрезка и вывод результата
+		return str[left:right], right + start
+	}
+
+	// Вывод ненайденных значений для тестов
+	// fmt.Println("error foundn't \t", subStr, "-")
+
+	return "", 0
+}
+
+// Облегчённая функция поиска. Возвращает только искомое значение
+func find(str, subStr, stopChar string) string {
+
+	// Проверка на существование нужной строки
+	if strings.Contains(str, subStr) {
+
+		// Обрезка левой части
+		str = str[strings.Index(str, subStr)+len(subStr):]
+
+		// Обрезка правой части и вывод результата
+		return str[:strings.Index(str, stopChar)]
+	}
+
+	return ""
+}
+
+// Функция проверки наличия подстроки
+func contains(str, subStr string, left int) bool {
+
+	return strings.Contains(str[left:], subStr)
+}
