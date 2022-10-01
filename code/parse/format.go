e2eb9b8d55d6d4a912e7db77cbc5c2e3a3432d0d
hud0shnik <c4nsero@mail.ru>
2022-10-01 16:44:02 +0300

    update parse: add format.go

---
+++ code/parse/format.go
@@ -0,0 +1,52 @@
+package parse
+
+import (
+	"fmt"
+	"strconv"
+)
+
+//	Функции перевода из строки в другие типы
+
+func ToInt(s string) int {
+	i, err := strconv.Atoi(s)
+
+	if err != nil {
+		fmt.Println("parsing error: \t", s)
+		return -1
+	}
+
+	return i
+}
+
+func ToInt64(s string) int64 {
+	i, err := strconv.ParseInt(s, 10, 64)
+
+	if err != nil {
+		fmt.Println("parsing error: \t", s)
+		return -1
+	}
+
+	return i
+}
+
+func ToBool(s string) bool {
+	f, err := strconv.ParseBool(s)
+
+	if err != nil {
+		fmt.Println("parsing error: \t", s)
+		return false
+	}
+
+	return f
+}
+
+func ToFloat64(s string) float64 {
+	i, err := strconv.ParseFloat(s, 64)
+
+	if err != nil {
+		fmt.Println("parsing error: \t", s)
+		return -1
+	}
+
+	return i
+}
