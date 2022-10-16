8edb4843f865975ecaaa30bf574fd28affeb3370
hud0shnik <c4nsero@mail.ru>
2022-10-16 12:43:16 +0300

    update parse: update onlineInfo

--- code/parse/format.go
+++ code/parse/format.go
@@ -2,8 +2,6 @@ package parse
 
 import (
 	"fmt"
-	"io/ioutil"
-	"net/http"
 	"strconv"
 	"strings"
 )
@@ -247,55 +245,6 @@ type BeatMapSet struct {
 type Weight struct {
 	Percentage float64 `json:"percentage"`
 	PP         float64 `json:"pp"`
-}
-
-// Статуса пользователя
-type OnlineInfo struct {
-	Error  string `json:"error"`
-	Status string `json:"is_online"`
-}
-
-// Функция получения информации о пользователе
-func GetOnlineInfo(id string) OnlineInfo {
-
-	// Если пользователь не ввёл id, по умолчанию ставит мой id
-	if id == "" {
-		id = "29829158"
-	}
-
-	// Формирование и исполнение запроса
-	resp, err := http.Get("https://osu.ppy.sh/users/" + id)
-	if err != nil {
-		return OnlineInfo{
-			Error: "http.Get error",
-		}
-	}
-
-	// Запись респонса
-	defer resp.Body.Close()
-	body, _ := ioutil.ReadAll(resp.Body)
-
-	// HTML полученной страницы в формате string
-	pageStr := string(body)[90000:]
-
-	// Сохранение html"ки в файл sample.html (для тестов)
-	/*
-		if err := os.WriteFile("sample.html", []byte(pageStr), 0666); err != nil {
-			log.Fatal(err)
-		}
-	*/
-
-	// Проверка на страницу пользователя
-	if strings.Contains(pageStr, "js-react--profile") {
-		return OnlineInfo{
-			Status: find(pageStr, "is_online&quot;:", ","),
-		}
-	}
-
-	return OnlineInfo{
-		Error: "User not found",
-	}
-
 }
 
 // Функция получения информации о пользователе
--- code/parse/parse.go
+++ code/parse/parse.go
@@ -341,6 +341,12 @@ type PlayCountBeatmapString struct {
 	Version          string `json:"version"`
 }
 
+// Статуса пользователя
+type OnlineInfo struct {
+	Error  string `json:"error"`
+	Status string `json:"is_online"`
+}
+
 // Функция для парсинга карты
 func parseBeatmapsString(pageStr string, left int) ([]BeatmapString, int) {
 
@@ -962,4 +968,47 @@ func GetUserInfoString(id, mode string) UserInfoString {
 	}
 
 	return result
+}
+
+// Функция получения информации о пользователе
+func GetOnlineInfo(id string) OnlineInfo {
+
+	// Если пользователь не ввёл id, по умолчанию ставит мой id
+	if id == "" {
+		id = "29829158"
+	}
+
+	// Формирование и исполнение запроса
+	resp, err := http.Get("https://osu.ppy.sh/users/" + id)
+	if err != nil {
+		return OnlineInfo{
+			Error: "http.Get error",
+		}
+	}
+
+	// Запись респонса
+	defer resp.Body.Close()
+	body, _ := ioutil.ReadAll(resp.Body)
+
+	// HTML полученной страницы в формате string
+	pageStr := string(body)[80000:]
+
+	// Сохранение html"ки в файл sample.html (для тестов)
+	/*
+		if err := os.WriteFile("sample.html", []byte(pageStr), 0666); err != nil {
+			log.Fatal(err)
+		}
+	*/
+
+	// Проверка на страницу пользователя
+	if strings.Contains(pageStr, "js-react--profile") {
+		return OnlineInfo{
+			Status: find(pageStr, "is_online&quot;:", ","),
+		}
+	}
+
+	return OnlineInfo{
+		Error: "User not found",
+	}
+
 }
