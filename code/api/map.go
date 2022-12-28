71eb5d66dd419be86934933aef9a93e852104811
Danila Egorov <c4nsero@mail.ru>
2022-12-28 17:55:35 +0300

    update map: update mapResponse structure

--- code/api/map.go
+++ code/api/map.go
@@ -8,28 +8,35 @@ import (
 
 // Структура респонса
 type MapResponse struct {
-	Error            string `json:"error"`
-	Artist           string `json:"artist"`
-	ArtistUnicode    string `json:"artist_string"`
-	Covers           Covers `json:"covers"`
-	Creator          string `json:"creator"`
-	FavoriteCount    int    `json:"favorite_count"`
-	HypeCurrent      string `json:"hype_current"`
-	HypeRequired     string `json:"hype_required"`
-	Id               int    `json:"id"`
-	Nsfw             bool   `json:"nsfw"`
-	Offset           int    `json:"offset"`
-	PlayCount        int    `json:"play_count"`
-	PreviewUrl       string `json:"preview_url"`
-	Source           string `json:"source"`
-	Spotlight        bool   `json:"spotlight"`
-	Status           string `json:"status"`
-	Title            string `json:"title"`
-	TitleUnicode     string `json:"title_unicode"`
-	TrackId          int    `json:"track_id"`
-	UserId           int    `json:"user_id"`
-	Video            bool   `json:"video"`
-	DownloadDisabled bool   `json:"download_disabled"`
+	Error             string  `json:"error"`
+	Artist            string  `json:"artist"`
+	ArtistUnicode     string  `json:"artist_string"`
+	Covers            Covers  `json:"covers"`
+	Creator           string  `json:"creator"`
+	FavoriteCount     int     `json:"favorite_count"`
+	HypeCurrent       string  `json:"hype_current"`
+	HypeRequired      string  `json:"hype_required"`
+	Id                int     `json:"id"`
+	Nsfw              bool    `json:"nsfw"`
+	Offset            int     `json:"offset"`
+	PlayCount         int     `json:"play_count"`
+	PreviewUrl        string  `json:"preview_url"`
+	Source            string  `json:"source"`
+	Spotlight         bool    `json:"spotlight"`
+	Status            string  `json:"status"`
+	Title             string  `json:"title"`
+	TitleUnicode      string  `json:"title_unicode"`
+	TrackId           int     `json:"track_id"`
+	UserId            int     `json:"user_id"`
+	Video             bool    `json:"video"`
+	DownloadDisabled  bool    `json:"download_disabled"`
+	Bpm               float64 `json:"bpm"`
+	CanBeHyped        bool    `json:"can_be_hyped"`
+	DiscussionEnabled bool    `json:"discussion_enabled"`
+	DiscussionLocked  bool    `json:"discussion_locked"`
+	IsScoreable       bool    `json:"is_scoreable"`
+	LastUpdated       string  `json:"last_updated"`
+	LegacyThreadUrl   string  `json:"legacy_thread_url"`
 }
 
 // Роут "/map" для vercel
