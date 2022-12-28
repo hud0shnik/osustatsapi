447ab2a6654cd571f5e300435a701fdd281790c8
Danila Egorov <c4nsero@mail.ru>
2022-12-28 17:51:24 +0300

    update map: update mapResponse struct

--- code/api/map.go
+++ code/api/map.go
@@ -8,17 +8,28 @@ import (
 
 // Структура респонса
 type MapResponse struct {
-	Error         string `json:"error"`
-	Artist        string `json:"artist"`
-	ArtistUnicode string `json:"artist_string"`
-	Covers        Covers `json:"covers"`
-	Creator       string `json:"creator"`
-	FavoriteCount int    `json:"favorite_count"`
-	HypeCurrent   string `json:"hype_current"`
-	HypeRequired  string `json:"hype_required"`
-	Id            int    `json:"id"`
-	Nsfw          bool   `json:"nsfw"`
-	Offset        int    `json:"offset"`
+	Error            string `json:"error"`
+	Artist           string `json:"artist"`
+	ArtistUnicode    string `json:"artist_string"`
+	Covers           Covers `json:"covers"`
+	Creator          string `json:"creator"`
+	FavoriteCount    int    `json:"favorite_count"`
+	HypeCurrent      string `json:"hype_current"`
+	HypeRequired     string `json:"hype_required"`
+	Id               int    `json:"id"`
+	Nsfw             bool   `json:"nsfw"`
+	Offset           int    `json:"offset"`
+	PlayCount        int    `json:"play_count"`
+	PreviewUrl       string `json:"preview_url"`
+	Source           string `json:"source"`
+	Spotlight        bool   `json:"spotlight"`
+	Status           string `json:"status"`
+	Title            string `json:"title"`
+	TitleUnicode     string `json:"title_unicode"`
+	TrackId          int    `json:"track_id"`
+	UserId           int    `json:"user_id"`
+	Video            bool   `json:"video"`
+	DownloadDisabled bool   `json:"download_disabled"`
 }
 
 // Роут "/map" для vercel
