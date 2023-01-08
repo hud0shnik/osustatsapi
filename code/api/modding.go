79ad4fc850e1191609c3b4413bc59f9e4b23e0ca
Danila Egorov <c4nsero@mail.ru>
2023-01-08 13:52:26 +0300

    update modding: add moddingUser struct

--- code/api/modding.go
+++ code/api/modding.go
@@ -100,6 +100,23 @@ type StartingPost struct {
 	UserId                 string `json:"user_id"`
 }
 
+// Структура пользователя
+type ModdingUser struct {
+	AvatarUrl     string `json:"avatar_url"`
+	CountryCode   string `json:"country_code"`
+	DefaultGroup  string `json:"default_group"`
+	Id            int    `json:"id "`
+	IsActive      bool   `json:"is_active"`
+	IsBot         bool   `json:"is_bot"`
+	IsDeleted     bool   `json:"is_deleted"`
+	IsOnline      bool   `json:"is_online"`
+	IsSupporter   bool   `json:"is_supporter"`
+	LastVisit     string `json:"last_visit"`
+	PmFriendsOnly bool   `json:"pm_friends_only"`
+	ProfileColor  string `json:"profile_color"`
+	Username      string `json:"username"`
+}
+
 // Функция получения текстовой информации
 func GetModdingInfoString(id string) ModdingResponseString {
 
