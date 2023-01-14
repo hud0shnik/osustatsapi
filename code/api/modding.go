3cf8c7ae0ed7c474397b2a967f7836038155ad60
Danila Egorov <c4nsero@mail.ru>
2023-01-14 11:27:43 +0300

    update modding: fix moddingUserString struct

--- code/api/modding.go
+++ code/api/modding.go
@@ -112,7 +112,7 @@ type ModdingUserString struct {
 	AvatarUrl     string        `json:"avatar_url"`
 	CountryCode   string        `json:"country_code"`
 	DefaultGroup  string        `json:"default_group"`
-	Id            int           `json:"id "`
+	Id            int           `json:"id"`
 	IsActive      bool          `json:"is_active"`
 	IsBot         bool          `json:"is_bot"`
 	IsDeleted     bool          `json:"is_deleted"`
