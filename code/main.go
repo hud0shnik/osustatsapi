5b52104f4f22c515b59b44f777df06032078bedf
Danila Egorov <c4nsero@mail.ru>
2023-02-03 22:50:53 +0300

    update main: optimize
    Убрал лишние функции из мейна
    Роуты Vercel оказывается отлично подходят для Mux

--- code/main.go
+++ code/main.go
@@ -1,7 +1,6 @@
 package main
 
 import (
-	"encoding/json"
 	"fmt"
 	"log"
 	"net/http"
@@ -11,56 +10,6 @@ import (
 
 	"github.com/gorilla/mux"
 )
-
-// Функция отправки информации о пользователе
-func sendUserInfo(writer http.ResponseWriter, request *http.Request) {
-
-	// Заголовок, определяющий тип данных респонса
-	writer.Header().Set("Content-Type", "application/json")
-
-	// Проверка на тип, обработка и вывод результата
-	if request.URL.Query().Get("type") == "string" {
-		json.NewEncoder(writer).Encode(api.GetUserInfoString(request.URL.Query().Get("id")))
-	} else {
-		json.NewEncoder(writer).Encode(api.GetUserInfo(request.URL.Query().Get("id")))
-	}
-}
-
-// Функция отправки информации о статусе пользователя
-func sendOnlineInfo(writer http.ResponseWriter, request *http.Request) {
-
-	// Заголовок, определяющий тип данных респонса
-	writer.Header().Set("Content-Type", "application/json")
-
-	// Обработка данных и вывод результата
-	json.NewEncoder(writer).Encode(api.GetOnlineInfo(request.URL.Query().Get("id")))
-}
-
-// Функция отправки информации о карте
-func sendMapInfo(writer http.ResponseWriter, request *http.Request) {
-
-	// Заголовок, определяющий тип данных респонса
-	writer.Header().Set("Content-Type", "application/json")
-
-	// Проверка на тип, обработка и вывод результата
-	if request.URL.Query().Get("type") == "string" {
-		json.NewEncoder(writer).Encode(api.GetMapInfoString(request.URL.Query().Get("beatmapset"), request.URL.Query().Get("id")))
-	} else {
-		json.NewEncoder(writer).Encode(api.GetMapInfo(request.URL.Query().Get("beatmapset"), request.URL.Query().Get("id")))
-	}
-
-}
-
-// Функция отправки информации о карте
-func sendModdingInfo(writer http.ResponseWriter, request *http.Request) {
-
-	// Заголовок, определяющий тип данных респонса
-	writer.Header().Set("Content-Type", "application/json")
-
-	// Обработка и вывод результата
-	json.NewEncoder(writer).Encode(api.GetModdingInfoString(request.URL.Query().Get("id")))
-
-}
 
 func main() {
 
@@ -73,13 +22,13 @@ func main() {
 
 	// Маршруты
 
-	router.HandleFunc("/api/user", sendUserInfo).Methods("GET")
+	router.HandleFunc("/api/user", api.User).Methods("GET")
 
-	router.HandleFunc("/api/online", sendOnlineInfo).Methods("GET")
+	router.HandleFunc("/api/online", api.Online).Methods("GET")
 
-	router.HandleFunc("/api/map", sendMapInfo).Methods("GET")
+	router.HandleFunc("/api/map", api.Map).Methods("GET")
 
-	router.HandleFunc("/api/modding", sendModdingInfo).Methods("GET")
+	router.HandleFunc("/api/modding", api.Modding).Methods("GET")
 
 	// Запуск API
 	log.Fatal(http.ListenAndServe(":"+os.Getenv("PORT"), router))
